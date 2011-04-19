c00966fbbabd324109e834bbea5ed83ae071cab4
Kragen Javier Sitaker <kragen@canonical.org>
2011-04-19 18:20:28 -0300

    Added an icon that appears on all pages

--- pncontent.js
+++ pncontent.js
@@ -1,1 +1,24 @@
-console.log("am ok, %o", $);
+var border = '1px solid black';
+var borderRadius = '3px';
+var iconSize = '32px';
+
+$('<div/>')
+  .css({ position: 'fixed'
+       , top: '10px'
+       , right: '0px'
+       , backgroundColor: 'white'
+       , borderTopLeftRadius: borderRadius
+       , borderBottomLeftRadius: borderRadius
+       , padding: '3px'
+       , borderTop: border
+       , borderLeft: border
+       , borderBottom: border
+       , zIndex: 2147483646
+       , opacity: 0.5
+       })
+  .append($('<img/>')
+          .attr('src', chrome.extension.getURL('write.png'))
+          .css({width: iconSize, height: iconSize})
+         )
+  .appendTo(document.body)
+  ;
